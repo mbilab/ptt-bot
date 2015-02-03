2538f0cf31fa13ef0149a789d2c177829b893174
張家誠 <chenchen16888@gmail.com>
2015-02-03 21:40:06 +0800

    set id and ps as private

--- mybot.js
+++ mybot.js
@@ -1,18 +1,28 @@
 var myBot = require('./PTT-BOT/ptt-bot');
 var fs = require('fs');
 var iconv = require('iconv-lite'); 
+var S = require('string');
 
-//create the connection object for robot.
-console.log('long time no see!');
-myBot.login('chengrobot','ps2014',function(){
-	console.log('hi, chenchen chang, how are you?');
-	//console.log(myBot.getScreen());
-	/*
-	fs.writeFile('screen_data/cursormove.txt', iconv.encode(myBot.getScreen(),'big5'), function (err) {
-		if (err) throw err;
-		console.log('cursormove is saved!');
+//get personal ID & PS from myID.txt
+fs.readFile('myID.txt',{encoding:'utf-8'}, function (err, data) {
+	
+	if (err) throw err;
+	id = S(data).between("ID:'","'").s;
+	ps = S(data).between("PS:'","'").s;
+	
+	//create the connection object for robot.
+	console.log('long time no see!');
+	myBot.login( id, ps, function(){
+		console.log('hi, chenchen chang, how are you?');
+		//console.log(myBot.getScreen());
+		/*
+		fs.writeFile('screen_data/cursormove.txt', iconv.encode(myBot.getScreen(),'big5'), function (err) {
+			if (err) throw err;
+			console.log('cursormove is saved!');
+		});
+		*/
 	});
-	*/
+	
 });
 
 
