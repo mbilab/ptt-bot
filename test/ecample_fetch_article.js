44f83d2756792a08982dc1cef0c18a1b8a074d88
張家誠 <bird11419@yahoo.com.tw>
2015-03-15 11:25:05 +0800

    add file: example_fetch_article

---
+++ test/ecample_fetch_article.js
@@ -0,0 +1,48 @@
+var myBot = require('./PTT-BOT/ptt-bot');
+var fs = require('fs');
+var iconv = require('iconv-lite'); 
+var S = require('string');
+
+//login
+fs.readFile('myID.txt',{encoding:'utf-8'}, function (err, data) {
+	
+	if (err) throw err;
+	id = S(data).between("ID:'","'").s;
+	ps = S(data).between("PS:'","'").s;
+	
+	//create the connection object for robot.
+	console.log('long time no see!');
+	myBot.login( id, ps, function(){
+		console.log('hi, chenchen chang, something about state is fixed ya');
+		console.log('下面應該要是主選單才對');
+		console.log(myBot.getScreen());
+		/*
+		fs.writeFile('screen_data/cursormove.txt', iconv.encode(myBot.getScreen(),'big5'), function (err) {
+			if (err) throw err;
+			console.log('cursormove is saved!');
+		});
+		*/
+	});
+	
+	myBot.toBoard('movie',function(){
+		console.log('下面應該要是電影版');
+		console.log(myBot.getScreen());
+	
+		fs.writeFile('screen_data/toBoard.txt', iconv.encode(myBot.getScreen(),'big5'), function (err) {
+			if (err) throw err;
+			console.log('toBoard is saved!');
+		});
+		console.log(myBot.where());
+	});
+	
+	
+	myBot.toArticle('52642',function(){ //or myBot.sendRight()
+		console.log(myBot.getScreen());
+		
+	});
+	
+	myBot.fetchArticle(function(){
+		console.log(myBot.getArticle());
+		console.log(myBot.where());
+	});
+});
